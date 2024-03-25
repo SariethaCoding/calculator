8e1d0806739b6d2234a85a382dc394de99edaba1
Sarietha <sarietha.coding@gmail.com>
2024-03-25 12:41:36 +0200

    Pages created and basic layout added

---
+++ calc.js
@@ -0,0 +1,28 @@
+
+
+function Add(num1, num2) {
+
+}
+
+
+
+
+
+
+
+function Operate(operator, num1, num2) {
+    switch(operator) {
+        case "add" :
+            return Add(num1, num2);
+            break;
+
+        case "subtract" :
+            break;
+
+        case "multiply" :
+            break;
+
+        case "divide"  :
+            break;
+    } 
+}
