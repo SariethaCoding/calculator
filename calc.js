c482c5174424299eb0b72b79e3703d95b7f66cae
Sarietha <sarietha.coding@gmail.com>
2024-03-25 13:40:21 +0200

    Added the listener event for click on buttons

--- calc.js
+++ calc.js
@@ -1,4 +1,27 @@
 
+// Calculator Display Text
+let calcDisplayText = "";
+
+
+const buttons = document.querySelectorAll("input[type='button']");
+
+//console.log("buttons :  " + buttons.length);
+
+buttons.forEach((button) => {
+    //console.log("this button i s: " + button);
+    button.addEventListener("click", function() {
+        updateCalcDisplay(this.value);
+    });
+    
+});
+
+function updateCalcDisplay(myValue) {
+    console.log(myValue);
+    calcDisplayText = calcDisplayText + myValue;
+    console.log(calcDisplayText);
+    document.getElementById("cDisplay").textContent = calcDisplayText;
+}
+
 
 function Add(num1, num2) {
 
